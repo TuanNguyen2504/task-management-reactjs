59390df8b2aeefc2b51a333e27f516db71894d90
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-07-14 17:48:14 +0700

    Updates

---
+++ precache-manifest.c568c556f16d888f55540ba483ddf361.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2224ec624e7b6212f78e6d72d5eddc8a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0b3ac55ec30398bdd02",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "973a28d392de5a698ef3",
+    "url": "./static/css/main.b7dae844.chunk.css"
+  },
+  {
+    "revision": "a0b3ac55ec30398bdd02",
+    "url": "./static/js/2.11e7f5fe.chunk.js"
+  },
+  {
+    "revision": "88e77cb223281485d18962ec7c3695d9",
+    "url": "./static/js/2.11e7f5fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973a28d392de5a698ef3",
+    "url": "./static/js/main.7319343a.chunk.js"
+  },
+  {
+    "revision": "740e4df4ea41bf485282",
+    "url": "./static/js/runtime-main.3c610b3f.js"
+  },
+  {
+    "revision": "7f608b96c1ed4f8f989f08f27207efdc",
+    "url": "./static/media/bin.7f608b96.svg"
+  },
+  {
+    "revision": "a726e4739ce2fb69b14a0686ce06c262",
+    "url": "./static/media/check-complete.a726e473.svg"
+  },
+  {
+    "revision": "5ff4dfc991ba2c7d8318dad2525d7e8a",
+    "url": "./static/media/check.5ff4dfc9.svg"
+  },
+  {
+    "revision": "db093ba043cccd5dd5196d92cf584801",
+    "url": "./static/media/close.db093ba0.svg"
+  },
+  {
+    "revision": "4119680fcf9296fb2a0b3ce04c85533b",
+    "url": "./static/media/cross.4119680f.svg"
+  },
+  {
+    "revision": "0024d862e84dad80e4abc46b09fe40c8",
+    "url": "./static/media/pencil.0024d862.svg"
+  },
+  {
+    "revision": "96f7f42cb655254e0f46dc4cf0d7ecab",
+    "url": "./static/media/plus.96f7f42c.svg"
+  },
+  {
+    "revision": "a525307cb64609429d7fddfea943998e",
+    "url": "./static/media/search.a525307c.svg"
+  },
+  {
+    "revision": "12573be80b93afb780e6d34034bab398",
+    "url": "./static/media/sort-a-z.12573be8.svg"
+  },
+  {
+    "revision": "88956179f92ec920ac9d7a28ddd07677",
+    "url": "./static/media/sort-z-a.88956179.svg"
+  }
+]);
